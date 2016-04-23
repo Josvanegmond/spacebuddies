5b6c13fb197ef6c120e0488a362ab0181583bcd4
josvanegmond <jostievanegmond@gmail.com>
2016-04-23 16:36:26 +0200

    Add accelerometer

--- source/core/src/nl/joozey/spaceapps/spacebuddies/SpaceBuddiesMain.java
+++ source/core/src/nl/joozey/spaceapps/spacebuddies/SpaceBuddiesMain.java
@@ -2,26 +2,42 @@ package nl.joozey.spaceapps.spacebuddies;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SpaceBuddiesMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import sun.rmi.runtime.Log;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+public class SpaceBuddiesMain extends ApplicationAdapter {
+
+    private static final String TAG = SpaceBuddiesMain.class.getName();
+
+    private SpriteBatch batch;
+    private Texture img;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("space.png");
+        img.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+
+        boolean available = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
+        System.out.println(TAG + " Accelerometer? " + available);
+    }
+
+    @Override
+    public void render() {
+        float accelX = Gdx.input.getAccelerometerX();
+        float accelY = Gdx.input.getAccelerometerY();
+        float accelZ = Gdx.input.getAccelerometerZ();
+
+        System.out.println(TAG + " Accel X:" + accelX + " Y:" + accelY + " Z:" + accelZ);
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 }
