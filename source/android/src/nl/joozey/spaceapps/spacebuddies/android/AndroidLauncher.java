b58ddd4d5b90e2dbf2288331430083ea1f61cbce
josvanegmond <jostievanegmond@gmail.com>
2016-04-23 15:53:38 +0200

    Initial project

---
+++ source/android/src/nl/joozey/spaceapps/spacebuddies/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package nl.joozey.spaceapps.spacebuddies.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import nl.joozey.spaceapps.spacebuddies.SpaceBuddiesMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceBuddiesMain(), config);
+	}
+}
