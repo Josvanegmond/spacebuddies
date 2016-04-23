2515e6b11f6b8af82c2876491ffbbdcbe281387a
josvanegmond <jostievanegmond@gmail.com>
2016-04-23 18:42:40 +0200

    Moon vector added

--- source/core/src/nl/joozey/spaceapps/spacebuddies/SpaceBuddiesMain.java
+++ source/core/src/nl/joozey/spaceapps/spacebuddies/SpaceBuddiesMain.java
@@ -3,9 +3,12 @@ package nl.joozey.spaceapps.spacebuddies;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 import sun.rmi.runtime.Log;
 
@@ -15,15 +18,28 @@ public class SpaceBuddiesMain extends ApplicationAdapter {
 
     private SpriteBatch batch;
     private Texture img;
+    private BitmapFont font;
+
+    private Vector2 moonVector;
 
     @Override
     public void create() {
+        font = new BitmapFont();
+        font.getData().setScale(3);
+
         batch = new SpriteBatch();
         img = new Texture("space.png");
         img.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
         boolean available = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
         System.out.println(TAG + " Accelerometer? " + available);
+
+        moonVector = XmlMoonParser.getMoonData();
+        if(moonVector != null) {
+            System.out.println(TAG + " MoonVector x:" + moonVector.x + " y:" + moonVector.y);
+        } else {
+            System.out.println(TAG + "MoonVector Null");
+        }
     }
 
     @Override
@@ -42,12 +58,21 @@ public class SpaceBuddiesMain extends ApplicationAdapter {
         angleY = (float)Math.toDegrees((double) angleY);
         angleZ = (float)Math.toDegrees((double) angleZ);
 
-        System.out.println(TAG + " Angle X:" + angleX + " Y:" + angleY + " Z:" + angleZ);
-
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+
         batch.draw(img, 0, 0);
+
+        font.setColor(Color.WHITE);
+        font.draw(batch, " Angle X:" + angleX + " Y:" + angleY + " Z:" + angleZ, 20, 60);
+
+        if(moonVector != null) {
+            font.draw(batch, " Moon Elevation:" + moonVector.x + " Azimuth:" + moonVector.y, 20, 110);
+        }
+
         batch.end();
+
+
     }
 }
--- source/core/src/nl/joozey/spaceapps/spacebuddies/XmlMoonParser.java
+++ source/core/src/nl/joozey/spaceapps/spacebuddies/XmlMoonParser.java
@@ -1,7 +1,52 @@
 package nl.joozey.spaceapps.spacebuddies;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.XmlReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 /**
  * Created by mint on 23-4-16.
  */
 public class XmlMoonParser {
+    public static Vector2 getMoonData() {
+        XmlReader xmlReader = new XmlReader();
+
+        try {
+            URL oracle = new URL("http://api.wolframalpha.com/v2/query?appid=2Q6RQG-58PVTW6AH6&input=position%20of%20moon%20in%20sky%20from%2052.213952°N%2C%204.3263°E&format=plaintext&podstate=Location:PlanetaryMoonData__Show+decimal");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
+
+            String xml = "";
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                xml += inputLine;
+            }
+            in.close();
+
+            XmlReader.Element xmlElement = xmlReader.parse(xml);
+            Array<XmlReader.Element> children = xmlElement.getChildrenByNameRecursively("plaintext");
+            String moonData = children.get(1).getText();
+            moonData = moonData.replace("right ascension | ", "");
+            moonData = moonData.replace("^hdeclination |", "");
+            moonData = moonData.replace("°", "");
+
+            String[] moonDataSplit = moonData.split(" ");
+            Vector2 moonVector = new Vector2(
+                    Float.parseFloat(moonDataSplit[0]),
+                    Float.parseFloat(moonDataSplit[1])
+            );
+
+            return moonVector;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
