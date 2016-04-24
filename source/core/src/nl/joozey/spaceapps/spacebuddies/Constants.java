35bc2a3a8213d9de2b448e67d9817ac42a132404
josvanegmond <jostievanegmond@gmail.com>
2016-04-24 09:50:05 +0200

    Paxi, orbit and smaller imges

--- source/core/src/nl/joozey/spaceapps/spacebuddies/Constants.java
+++ source/core/src/nl/joozey/spaceapps/spacebuddies/Constants.java
@@ -6,4 +6,5 @@ package nl.joozey.spaceapps.spacebuddies;
 public class Constants {
     public static final Screen FIND_MOON_SCREEN = new FindMoonScreen();
     public static final Screen GOTO_MOON_SCREEN = new GoToMoonScreen();
+    public static final Screen ORBIT_MOON_SCREEN = new OrbitMoonScreen();
 }
---
+++ source/core/src/nl/joozey/spaceapps/spacebuddies/OrbitMoonScreen.java
@@ -0,0 +1,58 @@
+package nl.joozey.spaceapps.spacebuddies;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+/**
+ * Created by mint on 24-4-16.
+ */
+public class OrbitMoonScreen implements Screen {
+    @Override
+    public void create(SpaceBuddiesMain main) {
+
+    }
+
+    @Override
+    public void render(Batch batch) {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
